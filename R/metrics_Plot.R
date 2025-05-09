259f40767e981586d25bd81fbce8004159371397
Martin Umpierrez <140851573+Martin-Umpierrez@users.noreply.github.com>
2025-05-08 21:45:08 -0300

    commit from MAC
    first commit from MAC

--- R/metrics_Plot.R
+++ R/metrics_Plot.R
@@ -10,6 +10,8 @@
 #'     \item \code{"bias_barplot"}: Bar plot of relative bias (\code{rBIAS}) with error bars.
 #'     \item \code{"MAIPE_barplot"}: Bar plot of MAIPE values.
 #'     \item \code{"bias_boxplot"}: Box plot of IPE values.
+#'     \item \code{"bias_dotplot"}: Dotplot of rBIAS values. Variability on individual bias
+#'     \item \code{"bias_density"}: Density Plot for rBias throughout occasions .
 #'     \item \code{"bias_violin"}: Violin plot of IPE values.
 #'     \item \code{"IF20_plot"}: Bar plot of IF20 values with reference line at 35%.
 #'     \item \code{"IF30_plot"}: Bar plot of IF30 values with reference line at 50%.
@@ -38,60 +40,80 @@
 #' }
 
 metrics_Plot <-
-function(mm, type = c('bias_barplot',
-                                     'MAIPE_barplot',
-                                     'bias_boxplot',
-                                     'bias_violin',
-                                     'IF20_plot',
-                                     'IF30_plot',)) {
-  pp <- NULL
+  function(mm, type = c('bias_barplot',
+                        'MAIPE_barplot',
+                        'bias_boxplot',
+                        'bias_violin',
+                        'bias_dotplot',
+                        'bias_density',
+                        'IF20_plot',
+                        'IF30_plot')) {
+    pp <- NULL
 
-  if (type == 'bias_barplot') {
-    pp <- mm[[2]] |>
-      mutate(OCC = factor(OCC) ) |>
-      ggplot( aes(x =OCC, y = rBIAS, fill = OCC) ) +
-      geom_col( ) +
-      geom_errorbar(aes(ymin = rBIAS_lower, ymax = rBIAS_upper), width = 0.2) +
-      geom_hline(data= data.frame(yy =c(-20, 20)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
-      scale_fill_brewer(palette = 'Dark2')
-  } else if (type == 'MAIPE_barplot') {
-    pp <-   mm[[2]] |>  # rBIAS_boxplot
-      mutate(OCC = factor(OCC) ) |>
-      ggplot (aes(x=OCC, y=MAIPE, fill=OCC)) + geom_boxplot() +
-      geom_hline(data= data.frame(yy =c(30)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
-      scale_fill_brewer(palette = 'Dark2')
-  } else if (type == 'bias_boxplot') {
-    pp <-   mm[[1]] |>  # rBIAS_boxplot
-      mutate(OCC = factor(OCC) ) |>
-      ggplot (aes(x=OCC, y=IPE, fill=OCC)) + geom_boxplot() +
-      geom_hline(data= data.frame(yy =c(-20, 20)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
-      scale_fill_brewer(palette = 'Dark2')
-  } else if (type == 'bias_violin') {
-    pp <- mm[[1]] |> # rBIAS_violinplot
-      mutate(OCC = factor(OCC) ) |>
-      ggplot( aes(x=OCC, y=IPE, fill=OCC)) + geom_violin() +
-      scale_fill_brewer(palette = 'Dark2')
-  } else if (type ==  'IF20_plot') {
-    pp <- mm[[2]] |> #  IF20_plot
-      mutate(OCC = factor(OCC) ) |>
-      ggplot(aes(x=OCC, y=IF20))+
-      geom_col( aes(fill=OCC) )+
-      geom_hline( aes(yintercept= 35), linetype = "dashed", colour= 'firebrick') +
-      scale_fill_brewer(palette = "Dark2")+
-      labs(title="IF20- Bayesian Forecasting",y="IF20(%)")+
-      theme(plot.title = element_text(size = rel(1), colour = "black")) +
-      theme(plot.title = element_text(size = 10, face = "bold"))
+    if (type == 'bias_barplot') {
+      pp <- mm[[2]] |>
+        mutate(OCC = factor(OCC) ) |>
+        ggplot( aes(x =OCC, y = rBIAS, fill = OCC) ) +
+        geom_col( ) +
+        geom_errorbar(aes(ymin = rBIAS_lower, ymax = rBIAS_upper), width = 0.2) +
+        geom_hline(data= data.frame(yy =c(-20, 20)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
+        scale_fill_brewer(palette = 'Dark2')
+    } else if (type == 'MAIPE_barplot') {
+      pp <-   mm[[2]] |>  # rBIAS_boxplot
+        mutate(OCC = factor(OCC) ) |>
+        ggplot (aes(x=OCC, y=MAIPE, fill=OCC)) + geom_boxplot() +
+        geom_hline(data= data.frame(yy =c(30)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
+        scale_fill_brewer(palette = 'Dark2')
+    } else if (type == 'bias_boxplot') {
+      pp <-   mm[[1]] |>  # rBIAS_boxplot
+        mutate(OCC = factor(OCC) ) |>
+        ggplot (aes(x=OCC, y=IPE, fill=OCC)) + geom_boxplot() +
+        geom_hline(data= data.frame(yy =c(-20, 20)), aes(yintercept= yy), linetype = "dashed", color='firebrick') +
+        scale_fill_brewer(palette = 'Dark2')
+
+    } else if (type == 'bias_dotplot') {
+      pp <- mm[[1]] |>
+        mutate(OCC = factor(OCC)) |>
+        ggplot(aes(x = OCC, y = IPE, color = OCC)) +
+        geom_jitter(width = 0.2, alpha = 0.6) +
+        geom_hline(data = data.frame(yy = c(-20, 20)), aes(yintercept = yy),
+                   linetype = "dashed", color = "firebrick") +
+        scale_color_brewer(palette = "Dark2") +
+        labs(y = "Individual Prediction Error (%)", title = "rBias dotplot")
+    } else if (type == 'bias_density') {
+      pp <- mm[[1]] |>
+        mutate(OCC = factor(OCC)) |>
+        ggplot(aes(x = IPE, fill = OCC)) +
+        geom_density(alpha = 0.4) +
+        geom_vline(xintercept = c(-20, 20), linetype = "dashed", color = "firebrick") +
+        scale_fill_brewer(palette = "Dark2") +
+        labs(x = "Individual Prediction Error (%)", title = " Distribution of rBias per OCC")
+    } else if (type == 'bias_violin') {
+      pp <- mm[[1]] |> # rBIAS_violinplot
+        mutate(OCC = factor(OCC) ) |>
+        ggplot( aes(x=OCC, y=IPE, fill=OCC)) + geom_violin() +
+        scale_fill_brewer(palette = 'Dark2')
+    } else if (type ==  'IF20_plot') {
+      pp <- mm[[2]] |> #  IF20_plot
+        mutate(OCC = factor(OCC) ) |>
+        ggplot(aes(x=OCC, y=IF20))+
+        geom_col( aes(fill=OCC) )+
+        geom_hline( aes(yintercept= 35), linetype = "dashed", colour= 'firebrick') +
+        scale_fill_brewer(palette = "Dark2")+
+        labs(title="IF20- Bayesian Forecasting",y="IF20(%)")+
+        theme(plot.title = element_text(size = rel(1), colour = "black")) +
+        theme(plot.title = element_text(size = 10, face = "bold"))
+    }
+    else if (type ==  'IF30_plot') {
+      pp <- mm[[2]] |> #  IF20_plot
+        mutate(OCC = factor(OCC) ) |>
+        ggplot(aes(x=OCC, y=IF30))+
+        geom_col( aes(fill=OCC) )+
+        geom_hline( aes(yintercept= 50), linetype = "dashed", colour= 'firebrick') +
+        scale_fill_brewer(palette = "Dark2")+
+        labs(title="IF30- Bayesian Forecasting",y="IF20(%)")+
+        theme(plot.title = element_text(size = rel(1), colour = "black")) +
+        theme(plot.title = element_text(size = 10, face = "bold"))
+    }
+    return(pp)
   }
-  else if (type ==  'IF30_plot') {
-    pp <- mm[[2]] |> #  IF20_plot
-      mutate(OCC = factor(OCC) ) |>
-      ggplot(aes(x=OCC, y=IF30))+
-      geom_col( aes(fill=OCC) )+
-      geom_hline( aes(yintercept= 50), linetype = "dashed", colour= 'firebrick') +
-      scale_fill_brewer(palette = "Dark2")+
-      labs(title="IF30- Bayesian Forecasting",y="IF20(%)")+
-      theme(plot.title = element_text(size = rel(1), colour = "black")) +
-      theme(plot.title = element_text(size = 10, face = "bold"))
-  }
-  return(pp)
-}
