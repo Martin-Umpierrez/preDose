99e21cb55a22c22b34ceda06dc49501b93e82a17
Martin Umpierrez <140851573+Martin-Umpierrez@users.noreply.github.com>
2026-01-05 16:01:26 -0300

    change in print metrics

--- R/print.EvalMetricsPPK.R
+++ R/print.EvalMetricsPPK.R
@@ -1,10 +1,13 @@
+#' @export
 print.EvalMetricsPPK <- function(x, ...) {
 
-  cat("Population Pkarmacokinetic Evaluation Metrics\n")
-  cat("======================\n")
+  cat("Population Pharmacokinetic Evaluation Metrics\n")
+  cat("============================================\n")
   cat("Evaluation type :", attr(x, "eval_type"), "\n")
   cat("Assessment      :", attr(x, "assessment"), "\n\n")
 
   cat("Mean metrics by OCC\n")
   print(x$metrics_means)
+
+  invisible(x)
 }
